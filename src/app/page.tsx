8da5004e78017702391d90dd94240b78bc0d6df1
Zenitsu-god <pranavlande1@gmail.com>
2025-01-14 13:38:35 +0530

    basic styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import { jet, satisfy } from "./fonts";
 import Featured from "@/components/home/featured";
 import Card from "@/components/home/card";
 import foodProducts from "@/lib/constants";
-import { context } from "@/context/contextProvider";
-import { useContext } from "react";
+import { FaFireFlameCurved } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <div className={"w-full h-full"}>
       <Featured />
       <section className="w-full">
+        <p className="flex gap-2 items-center mb-5 border-l-4 border-white px-3">
+          Trending
+          <FaFireFlameCurved className="w-5 h-5 fill-red-500" />
+        </p>
         <div className="w-full h-[300px] flex gap-5  overflow-x-auto">
           {foodProducts.map((item)=>(
               <Card key={item.title} image={item.image} title={item.title} description={item.description} price={item.price} />
