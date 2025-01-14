64bdfc26c743982d3a15dcba7e24abddae80e2a3
Rohit Ramteke <ramteker284@gmail.com>
2025-01-15 00:28:12 +0530

    Version-1.0.11 - Font Commented-Err

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,22 +1,22 @@
 import {JetBrains_Mono, Satisfy, Cutive_Mono,  Geist, Geist_Mono } from 'next/font/google';
 
-export const jet = JetBrains_Mono({
-    weight: "400",
-    subsets: ["latin", "cyrillic"],
-    display: 'swap'
-});
+// export const jet = JetBrains_Mono({
+//     weight: "400",
+//     subsets: ["latin", "cyrillic"],
+//     display: 'swap'
+// });
 
-export const cutive = Cutive_Mono({
-    weight:'400',
-    subsets: ["latin", "cyrillic"],
-    display: 'swap'
-});
+// export const cutive = Cutive_Mono({
+//     weight:'400',
+//     subsets: ["latin", "cyrillic"],
+//     display: 'swap'
+// });
 
-export const satisfy = Satisfy({
-    weight: "400",
-    subsets: ["latin", "cyrillic"],
-    display: 'swap'
-});
+// export const satisfy = Satisfy({
+//     weight: "400",
+//     subsets: ["latin", "cyrillic"],
+//     display: 'swap'
+// });
 
 export const geistSans = Geist({
     variable: "--font-geist-sans",
