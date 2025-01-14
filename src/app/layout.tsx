38466a3f579e9ea5809a3c5a471a39bc13ac66ef
Zenitsu-god <pranavlande1@gmail.com>
2025-01-14 16:37:02 +0530

    layout fix, scrollbar tinkering, card styling, featured css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,11 @@ export default function RootLayout({
           disableTransitionOnChange
         > */}
         <ContextProvider>
-          <div className={"w-screen h-screen bg-white text-white relative " + jet.className}>
-            <div className="w-full h-full relative bg-gradient-to-br from-gray-800 via-gray-900 to-gray-950">
-              <div className="absolute z-10 inset-0 bg-gradient-to-r from-white/5 to-transparent backdrop-blur-xl">
+          <div className={"min-w-screen min-h-screen scrollbar bg-white text-white relative " + jet.className}>
+            <div className="min-w-screen min-h-screen bg-gradient-to-br from-gray-800 via-gray-900 to-gray-950">
+              <div className="absolute w-full h-full z-10 inset-0 bg-gradient-to-r from-white/5 to-transparent backdrop-blur-xl">
               </div>
-                <div className="w-[95%] md:w-[90%] lg:w-[85%] xl:w-[80%] mx-auto relative z-20 p-8 text-white">
+                <div className="w-[95%] md:w-[90%] lg:w-[85%] xl:w-[80%] mx-auto relative z-20 p-4 sm:p-8 text-white">
                   <Navbar />
                   {children}
                 </div>
--- src/components/home/featured.jsx
+++ src/components/home/featured.jsx
@@ -5,19 +5,19 @@ import Link from "next/link";
 
 export default function Featured() {
     return(
-        <section className={"flex w-full flex-col sm:flex-row-reverse sm:justify-evenly py-4 "}>
-            <div className="w-full flex items-start justify-end relative">
-                <div className="h-full flex justify-end rounded-[100%]">
-                    <Image src="/featured_food.png" alt="featuredimg" quality={100} fill className="object-contain" />
+        <section className="flex w-full flex-col sm:flex-row-reverse sm:justify-evenly py-4">
+            <div className="w-full flex justify-center">
+                <div className="w-2/3 h-[200px] sm:h-full sm:w-full relative -left-5 sm:-left-0 ">
+                    <Image src="/featured_food.png" alt="featuredimg" quality={100} fill className="object-cover" />
                 </div>
             </div>
-            <div className="text-start w-full text-wrap pr-1/4">
-                <h1 className={"text-nowrap leading-tight tracking-widest " +  satisfy.className}>
+            <div className="text-start w-full text-wrap">
+                <h1 className={"text-nowrap text-center sm:text-left w-full leading-tight tracking-wide sm:tracking-widest " +  satisfy.className}>
                     New In Town <br />
                     New Delicasies. <br />
                     New Stories
                 </h1>
-                <div className="flex justify-start gap-6 py-6">
+                <div className="flex justify-center sm:justify-start w-full gap-6 py-6">
                     <Link href={"/menu"}>
                         <Button className="bg-red-500 rounded-full">
                             Menu
